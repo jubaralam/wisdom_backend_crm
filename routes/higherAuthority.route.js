e79ff46cabaaeac13757182dd52607c9ae019d8c
Zubair Alam <zubairalam@Zubairs-MacBook-Air.local>
2025-01-23 01:53:01 +0530

    higher Authority route has been created where three roles are included 'admin', 'manager', 'auditor' there are the roles can get all users, update and delete any of them in addition can get all customers, search by name email, phone and company also can update and delete any of them customer

--- routes/higherAuthority.route.js
+++ routes/higherAuthority.route.js
@@ -2,6 +2,7 @@ const express = require("express");
 const higherAuthorityRouter = express.Router();
 
 const UserModel = require("../models/user.model");
+const CustomerModel = require("../models/customer.model");
 
 higherAuthorityRouter.get("/users", async (req, res) => {
   const { page = 1, limit = 10 } = req.query;
@@ -73,6 +74,125 @@ higherAuthorityRouter.delete("/user/delete/:id", async (req, res) => {
     }
 
     res.status(200).send({ message: "User deleted successfully." });
+  } catch (error) {
+    res
+      .status(500)
+      .send({ message: "An error occurred. Please try again later." });
+  }
+});
+
+// search adn filter customers
+higherAuthorityRouter.get("/customer/search", async (req, res) => {
+  const { name, email, phone, company, page = 1, limit = 10 } = req.query;
+
+  try {
+    const query = {};
+    if (name) query.name = { $regex: name, $options: "i" }; // case-insensitive search
+    if (email) query.email = { $regex: email, $options: "i" }; // case-insensitive search
+    if (phone) query.phone = { $regex: phone, $options: "i" }; // case-insensitive search
+    if (company) query.company = { $regex: company, $options: "i" }; // case-insensitive search
+
+    const customers = await CustomerModel.find(query)
+      .skip((page - 1) * limit)
+      .limit(parseInt(limit));
+
+    if (customers.length === 0) {
+      return res.status(404).send({ message: "No customers found" });
+    }
+
+    res.status(200).send({ data: customers });
+  } catch (error) {
+    res
+      .status(500)
+      .send({ message: "An error occurred.", error: error.message });
+  }
+});
+
+//get all customers
+higherAuthorityRouter.get("/customer/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const customer = await CustomerModel.findById({ _id: id });
+    if (!customer) {
+      return res.status(404).send({ message: "customer not found" });
+    }
+    // Check if the user making the update is the owner of the customer
+
+    res.status(200).send({ data: customer });
+  } catch (error) {
+    res
+      .status(500)
+      .send({ message: "An error occurred. Please try again later." });
+  }
+});
+higherAuthorityRouter.get("/customers", async (req, res) => {
+  const user_id = req.user._id;
+  const { page = 1, limit = 10 } = req.query;
+  try {
+    const customers = await CustomerModel.find({ user_id })
+      .skip((page - 1) * limit)
+      .limit(parseInt(limit));
+    if (customers.length === 0) {
+      return res.status(404).send({ message: "No customers found" });
+    }
+
+    res.status(200).send({ data: customers });
+  } catch (error) {
+    res
+      .status(500)
+      .send({ message: "An error occurred. Please try again later ." });
+  }
+});
+
+// Update customer route
+higherAuthorityRouter.put("/customer/update/:id", async (req, res) => {
+  const { id } = req.params;
+  const { name, email, phone_no, company } = req.body;
+
+  try {
+    const customer = await CustomerModel.findById({ _id: id });
+
+    if (!customer) {
+      return res.status(404).send({ message: "Customer not found." });
+    }
+
+    let updatedFields = {};
+    if (name) updatedFields.name = name;
+    if (email) updatedFields.email = email;
+    if (phone_no) updatedFields.phone_no = phone_no;
+    if (company) updatedFields.company = company;
+
+    const updated = await CustomerModel.findByIdAndUpdate(id, updatedFields, {
+      new: true,
+    });
+    if (!updated) {
+      return res.status(404).send({ message: "customer not found." });
+    }
+
+    res.status(200).send({
+      message: "customer data has been updated successfully.",
+      customer: updated,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .send({ message: "An error occurred. Please try again later." });
+  }
+});
+
+// Delete user route
+higherAuthorityRouter.delete("/customer/delete/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deletedCustomer = await CustomerModel.findByIdAndDelete(id);
+    if (!deletedCustomer) {
+      return res.status(404).send({ message: "customer not found." });
+    }
+
+    res
+      .status(200)
+      .send({ message: "customer has been deleted successfully." });
   } catch (error) {
     res
       .status(500)
